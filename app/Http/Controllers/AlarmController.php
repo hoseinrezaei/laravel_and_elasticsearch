8a9bcf3bb5360d0983f7b0bd76a15c3ec12bc0d5
hosein rezaei <hosein.rezaei72@gmail.com>
2024-02-16 19:18:04 +0330

    add alarm for user store method is ok

--- app/Http/Controllers/AlarmController.php
+++ app/Http/Controllers/AlarmController.php
@@ -2,9 +2,39 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Alarm;
 use Illuminate\Http\Request;
 
 class AlarmController extends Controller
 {
-    //
+    public function store(Request $request){
+
+        // check validations
+        $validation = true;
+
+        $userAlarmCount = Alarm::where('user_id', $request->user_id)->count();
+
+        if ($validation == true && $userAlarmCount < 10){
+            $alarm = new Alarm();
+            $alarm->user_id = $request->user_id;
+            $alarm->platform = $request->platform;
+            $alarm->source = $request->source;
+            $alarm->save();
+
+            $data = [
+                'message' => 'alarm create successful',
+                'id' => $alarm->id
+            ];
+
+            return response()->json($data, 200);
+        }
+
+        $data = [
+            'message' => 'Error! bad request',
+            'id' => null
+        ];
+
+        return response()->json($data, 400);
+
+    }
 }
--- database/migrations/2024_02_16_143640_create_alarms_table.php
+++ database/migrations/2024_02_16_143640_create_alarms_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('alarms', function (Blueprint $table) {
             $table->id();
             $table->unsignedBigInteger('user_id')->nullable();
+            $table->string('platform')->nullable();
             $table->string('source')->nullable();
             $table->timestamps();
         });
