65449378a71773e54ef8a94acfb4929a531cfab6
hosein rezaei <hosein.rezaei72@gmail.com>
2024-02-16 18:11:34 +0330

    edit instagram migration file

--- database/migrations/2024_02_16_143441_create_instagrams_table.php
+++ database/migrations/2024_02_16_143441_create_instagrams_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('instagrams', function (Blueprint $table) {
             $table->id();
+            $table->date('date')->nullable();
+            $table->string('post_type')->nullable();
+            $table->string('source_username')->nullable();
+            $table->text('text')->nullable();
+            $table->string('link')->nullable();
             $table->timestamps();
         });
     }
